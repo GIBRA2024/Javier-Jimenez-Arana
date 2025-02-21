1cb8c2054088d2ea99d2b53c4f7beecd7f5dc051
Mateo <monorymateo1@gmail.com>
2025-02-21 18:15:12 -0500

    Cambio vcard

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 function descargarContacto() {
     const vcardData = `BEGIN:VCARD
 VERSION:3.0
+N:Jimenez Arana;Javier;;;
 FN:Javier Jimenez Arana
 TEL:+57 315 5843205
 EMAIL;TYPE=WORK: gerencia@cervalle.com
+ADR;TYPE=WORK:;;Cra. 29b #11-90;Acopi;Yumbo;;Colombia
 END:VCARD`;
 
     const blob = new Blob([vcardData], { type: 'text/vcard' });
